9d0df2badc284f14f744cef4a9794fae79e7241d
david <muwwy69@gmail.com>
2026-04-07 13:34:53 +0500

    init: configure Spring Boot, Gradle dependencies and basic settings

---
+++ src/main/java/com/normilinet/otklik/OtklikApplication.java
@@ -0,0 +1,11 @@
+package com.normilinet.otklik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtklikApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OtklikApplication.class, args);
+    }
+}
